7d39e19058ea4c41a1715dde8e4fbcc9ef0ed83c
Eknathreddy09 <eknath.reddy09@gmail.com>
2022-02-14 08:15:17 +0530

    Create start-session.sh

---
+++ workshop/setup.d/start-session.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+kubectl config delete-context eduk8s
+mv ~/config ~/.kube/config
+tanzu login --kubeconfig ~/.kube/config --context tkg-mgmttest-cluster-admin@tkg-mgmttest-cluster --name tkg-mgmttest-cluster
+kubectl config set-context tkg-mgmttest-cluster-admin@tkg-mgmttest-cluster
+kubectl config use-context tkg-mgmttest-cluster-admin@tkg-mgmttest-cluster
+kubectl config get-contexts
+tanzu mc get
